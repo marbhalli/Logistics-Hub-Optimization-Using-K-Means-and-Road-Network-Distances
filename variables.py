a09ab6fae3f67c4758fc8c437f494c5b7139f79f
marbhalli <abdulrehmanbhalli@live.com>
2022-07-09 23:40:17 +0500

    commiting variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+BOOL_WEIGHT = True
+
+GRID_SIZE = 1000
+
+n_iterations = 10
+
+cutoff = 10
+
+file_name = 'population_centroids_duel_weights_alpha_0.1'
+
+starting_centroids = [[8275352.13,3705453.333], [8276336.974,3698809.62],[8274113.367,3691933.585]]
